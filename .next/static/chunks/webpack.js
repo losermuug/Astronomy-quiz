00f2e03bb738b1cfc1e139c7a99f5cb3e8efe8c5
Muugii0101 <one.munkhzul@gmail.com>
2025-12-19 00:54:57 +0800

    tailwind deleted

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58927ab8ab824201"; }
+/******/ 		__webpack_require__.h = function() { return "fc30993be43f2d56"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
